dfc374b56bc4e7c590e5325df1a5e19246de8b6d
Balakrishnan Vijayabalan <vijayabalan_balakrishnan@carrefour.eu>
2023-01-06 15:32:35 +0100

    update

--- gcp-cloud/ops-agent/main.tf
+++ gcp-cloud/ops-agent/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_compute_instance" "server1" {
   name         = "test"
-  machine_type = "e2-medium"
+  machine_type = "e2-standard-2"
   zone         = "us-central1-a"
 
   tags = ["foo", "bar"]
